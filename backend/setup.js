08570c64031c4a840381c806fb3d7013d635f25c
Amir Keren <amir.k@taboola.com>
2019-04-03 17:06:55 +0300

    Add backend

---
+++ backend/setup.js
@@ -0,0 +1,24 @@
+var mysql = require('mysql');
+var fs = require('fs');
+
+var myCon = mysql.createConnection({
+    // host     : 'sql7.freemysqlhosting.net',
+    // user     : 'sql7278370',
+    // password : 'IAQpVk2u2N',
+    // database : 'sql7278370'
+    host: 'localhost',
+    port: '3306',
+    database: 'db',
+    user: 'user',
+    password: 'password'
+});
+
+var contents = fs.readFileSync('setup.sql', 'utf8').split('\n');
+for (var i = 0; i < contents.length; i++) {
+    if (contents[i]) {
+        console.log(contents[i]);
+        myCon.query(contents[i], function(err) {
+            if(err) console.log(err);
+        });
+    }
+}
