8fe32a05e544c37dec97e1d0bbbb256cbe18d33b
Gal Bari <gal.b@taboola.com>
2019-04-29 12:15:13 +0300

    fix winner

--- soccer-league/src/Knockout.js
+++ soccer-league/src/Knockout.js
@@ -17,39 +17,45 @@ class Knockout extends Component {
 				</div>
 			);
 		}
+
+		function UpdateScore(props) {
+			const { home_team, away_team, home_scored, away_scored, step_id, isAdmin } = props;
+			let { index } = props;
+			return (
+				<div>
+					{' '}
+					<Link
+						to={{
+							pathname: '/scores',
+							state: {
+								home_team: home_team,
+								away_team: away_team,
+								home_scored: home_scored,
+								away_scored: away_scored,
+								isKnockoutMatch: true,
+								step_id: step_id,
+								gameIndex: ++index,
+								isAdmin: isAdmin
+							}
+						}}
+					>
+						<div>Update score</div>
+					</Link>
+				</div>
+			);
+		}
 		const { isAdmin } = this.props;
 		const matches = this.props.games
 			? this.props.games.map((game, index) => {
-					const { home_team, away_team, home_scored, away_scored, step_id } = game;
-					// key={generateKey(Math.floor(Math.random() * Math.floor(100)))}
+					const { home_team, away_team, home_scored, away_scored } = game;
+					const randomKey = Math.floor(Math.random() * Math.floor(10000));
 					return (
-						<div className="matchContainer">
+						<div key={generateKey(randomKey)} className="matchContainer">
 							<div className="matchTeams">
 								<MatchTeam teamName={home_team} scored={home_scored} won={home_scored > away_scored} />
 								<MatchTeam teamName={away_team} scored={away_scored} won={home_scored < away_scored} />
 							</div>
-							{isAdmin && (
-								<div>
-									{' '}
-									<Link
-										to={{
-											pathname: '/scores',
-											state: {
-												home_team: home_team,
-												away_team: away_team,
-												home_scored: home_scored,
-												away_scored: away_scored,
-												isKnockoutMatch: true,
-												step_id: step_id,
-												gameIndex: ++index,
-												isAdmin: isAdmin
-											}
-										}}
-									>
-										<div>Update score</div>
-									</Link>
-								</div>
-							)}
+							{isAdmin && <UpdateScore {...game} index={index} />}
 						</div>
 					);
 			  })
--- soccer-league/src/Winner.js
+++ soccer-league/src/Winner.js
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Winner = function(props) {
-  const winnerTeam = props.games ? props.games[0].home : 'TBD'; 
+	const winnerTeam = props.games && props.games[0].home_team ? props.games[0].home_team : 'TBD';
+	const hasWinner = winnerTeam !== 'TBD';
+
 	const winner = (
-		<div className="match">
-			<div className="flag">{winnerTeam}</div>
+		<div className="matches">
+			<div className={`${hasWinner && 'champions'}`}>
+				<div className="flag">{winnerTeam}</div>
+			</div>
 		</div>
 	);
 
