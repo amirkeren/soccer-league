2be36b011ba4e1d4c3e750eb20da2ca016fcc7da
Amir Keren <amir.k@taboola.com>
2019-05-17 21:52:08 +0300

    Add players

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -184,8 +184,16 @@ router.get('/league/teams', function(req, res) {
             res.status(500).send({ "error": "can't load teams" });
             return;
         }
+        let teams = {};
+        for (let i = 0; i < results.length; i++) {
+            if (results[i].team_id in teams) {
+                teams[results[i].team_id].players.push(results[i].player_name);
+            } else {
+                teams[results[i].team_id] = { 'name': results[i].name, 'group_id': results[i].group_id, 'players': [ results[i].player_name ] };
+            }
+        }
         res.contentType('application/json');
-        res.send(results);
+        res.send(teams);
     });
 });
 
